d26fc9ed694a70aaf3c1f46280e6cc64c55fdfe6
T0ny-dev <antonioraweb@gmail.com>
2024-05-02 22:44:25 -0600

    update tarjets business

--- src/app/yulissa/page.jsx
+++ src/app/yulissa/page.jsx
@@ -8,10 +8,10 @@ function Tony () {
     <div className="tarjet">
       <BusinessCard
         img={"/logo-nexus.svg"}
-        nombre={"Tony Avilés"}
-        puesto={"CTO - Director de Tecnologias"}
-        especialidad={"Full-Stack Developer, Tecnologías educativas e Ingeniería Industrial"}
-        correo={"antonioraweb@gmail.com"}
+        nombre={"Yulissa Plata"}
+        puesto={"Directora de mercadotecnia"}
+        especialidad={"Marketing y relaciones publicas"}
+        correo={"yulissa-mariel@hotmail.com"}
       />
       <Video/>
     </div>
