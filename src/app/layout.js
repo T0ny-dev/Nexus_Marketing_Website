a5b77869c8d6aa58c5ef6631a13a90320379ceb6
T0ny-dev <antonioraweb@gmail.com>
2024-05-26 15:08:36 -0600

    vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import NavbarMobiles from "./components/NavbarMobiles";
 import { Inter } from 'next/font/google'
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react"
  
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ['latin'] })
@@ -25,6 +26,7 @@ export default function AppLayout({ children }) {
       <meta property="og:image" content="/ia_nexus_chat.png"/>
       </head>
       <body>
+      <Analytics />
         <Header />
         <NavbarMobiles/>
         <main className={inter.className}>{children}</main>
